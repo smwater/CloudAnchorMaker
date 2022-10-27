95a0fcb8cefa13bc40925e890df304c512643fae
Hyunsoo Park <smwater130@naver.com>
2022-10-27 17:04:19 +0900

    [Refactor] 코드 수정

--- Assets/Scripts/Data/CreateCubeUseIDButton.cs
+++ Assets/Scripts/Data/CreateCubeUseIDButton.cs
@@ -10,22 +10,6 @@ public class CreateCubeUseIDButton : MonoBehaviour
     public ARAnchorManager ARAnchorManager;
     public DataManager DataManager;
 
-    private GameObject Cube;
-    private bool a = false;
-
-    private void Update()
-    {
-        if (!a)
-        {
-            return;
-        }
-
-        if (Cube.transform.position != Vector3.zero)
-        {
-            Debug.Log(Cube.transform.position);
-        }
-    }
-
     /// <summary>
     /// 클라우드 앵커 ID를 이용해 앵커가 존재하는 지점에 큐브를 생성하는 메서드
     /// </summary>
@@ -45,10 +29,7 @@ public class CreateCubeUseIDButton : MonoBehaviour
             ARCloudAnchor arCloudAnchor = ARAnchorManager.ResolveCloudAnchorId(DataManager.GetAnchorID(i));
 
             // 클라우드 앵커가 존재하는 위치에 큐브를 생성
-            Cube = Instantiate(CubePrefab, arCloudAnchor.transform);
             Debug.Log("CloudAnchorID를 기반으로 큐브를 생성했습니다.");
         }
-
-        a = true;
     }
 }
